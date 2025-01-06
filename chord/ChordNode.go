78a835d97c112afaa742228793712629816503d0
Suchit <suchitkarunakaran@gmail.com>
2025-01-07 00:22:29 +0530

    Implement chord protocol methods

---
+++ chord/ChordNode.go
@@ -0,0 +1,128 @@
+package chord
+
+import (
+	"context"
+	"crypto/sha1"
+	"encoding/hex"
+	"log"
+	"math/big"
+	"time"
+
+	pb "github.com/suchit07-git/chordkv/rpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const nBits = 32
+
+type ChordNode struct {
+	id          int64
+	address     string
+	port        int32
+	fingerTable map[int]*ChordNode
+	predecessor *ChordNode
+	successor   *ChordNode
+	kvstore     map[string]string
+}
+
+func (node *ChordNode) FindSuccessor(id int64) *ChordNode {
+	if id >= node.id && id <= node.successor.id {
+		return node.successor
+	}
+	closestNode := node.ClosestPrecedingNode(id)
+	conn, err := grpc.NewClient(closestNode.address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect to node: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewChordServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	response, err := client.FindSuccessor(ctx, &pb.FindSuccessorRequest{Id: id})
+	if err != nil {
+		log.Fatalf("Couldn't find successor: %v", err)
+	}
+	return &ChordNode{id: response.Id, address: response.Address, port: response.Port}
+}
+
+func (node *ChordNode) ClosestPrecedingNode(id int64) *ChordNode {
+	for i := nBits - 1; i >= 0; i-- {
+		if node.fingerTable[i] != nil {
+			if node.fingerTable[i].id >= node.id && node.fingerTable[i].id <= id {
+				return node.fingerTable[i]
+			}
+		}
+	}
+	return node
+}
+
+func (node *ChordNode) Notify(n *ChordNode) {
+	if node.predecessor == nil || (n.id >= node.predecessor.id && n.id <= node.id) {
+		node.predecessor = n
+	}
+}
+
+func (node *ChordNode) Store(key string, value string) {
+	node.kvstore[key] = value
+}
+
+func sha1Hash(key string) int64 {
+	hashser := sha1.New()
+	hashser.Write([]byte(key))
+	hashBytes := hashser.Sum(nil)
+	hashHex := hex.EncodeToString(hashBytes)
+	hashBigInt := new(big.Int)
+	hashBigInt.SetString(hashHex, 16)
+	modulus := new(big.Int).SetUint64(1 << 32)
+	hashMod := new(big.Int).Mod(hashBigInt, modulus)
+	return hashMod.Int64()
+}
+
+func (node *ChordNode) Retrieve(key string) string {
+	hash_key := sha1Hash(key)
+	responsibleNode := node.FindSuccessor(hash_key)
+	if responsibleNode.id == node.id {
+		return node.kvstore[key]
+	}
+	conn, err := grpc.NewClient(responsibleNode.address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect to node: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewChordServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	response, err := client.Get(ctx, &pb.GetRequest{Key: key})
+	if err != nil {
+		log.Fatalf("Coudn't retrieve value for key %s: %v", key, err)
+	}
+	return response.Value
+}
+
+func (node *ChordNode) Join(bootstrapNode *ChordNode) {
+	if bootstrapNode != nil {
+		conn, err := grpc.NewClient(bootstrapNode.address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			log.Fatalf("Failed to connect to node: %v", err)
+		}
+		defer conn.Close()
+		client := pb.NewChordServiceClient(conn)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		response, err := client.FindSuccessor(ctx, &pb.FindSuccessorRequest{Id: node.id})
+		if err != nil {
+			log.Fatalf("Couldn't find successor: %v", err)
+		}
+		node.successor = &ChordNode{id: response.Id, address: response.Address, port: response.Port}
+		node.FixFingers()
+	} else {
+		node.successor = node
+	}
+	node.predecessor = nil
+}
+
+func (node *ChordNode) FixFingers() {
+	for i := 0; i < nBits; i++ {
+		node.fingerTable[i] = node.FindSuccessor((node.id + (1 << i)) % (1 << nBits))
+	}
+}
